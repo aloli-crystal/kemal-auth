4b2c9bc45785c4ccd1579e5e31765967eef60c22
Philippe Nénert <philippe@aloli.fr>
2026-03-09 14:27:02 -0400

    feat: règles CNIL pour les mots de passe (12 car, maj, min, chiffre, spécial)
    Mise à jour de KemalAuth::Password.validate :
    - 12 caractères minimum (au lieu de 8)
    - Au moins une lettre majuscule
    - Au moins une lettre minuscule (nouvelle règle)
    - Au moins un chiffre
    - Au moins un caractère spécial non alphanumérique (nouvelle règle)
    
    Conforme aux recommandations CNIL pour les mots de passe.

--- src/kemal_auth/password.cr
+++ src/kemal_auth/password.cr
@@ -8,6 +8,9 @@ module KemalAuth
     # Coût BCrypt par défaut (12 = bon compromis sécurité/performance)
     DEFAULT_COST = 12
 
+    # Longueur minimale selon les recommandations CNIL
+    MIN_LENGTH = 12
+
     # Hache un mot de passe en clair et retourne le hash BCrypt.
     #
     # ```
@@ -15,23 +18,32 @@ module KemalAuth
     # ```
     def self.hash(plain_password : String, cost : Int32 = DEFAULT_COST) : String
       raise ArgumentError.new("Le mot de passe ne peut pas être vide") if plain_password.empty?
-      raise ArgumentError.new("Le mot de passe doit contenir au moins 8 caractères") if plain_password.size < 8
+      raise ArgumentError.new("Le mot de passe doit contenir au moins #{MIN_LENGTH} caractères") if plain_password.size < MIN_LENGTH
       BCrypt::Password.create(plain_password, cost: cost).to_s
     end
 
-    # Vérifie la robustesse d'un mot de passe.
+    # Vérifie la robustesse d'un mot de passe selon les recommandations CNIL.
     # Retourne un tableau de messages d'erreur (vide si valide).
+    #
+    # Règles :
+    # - 12 caractères minimum
+    # - Au moins une lettre majuscule
+    # - Au moins une lettre minuscule
+    # - Au moins un chiffre
+    # - Au moins un caractère spécial (non alphanumérique)
     #
     # ```
     # errors = KemalAuth::Password.validate("abc")
-    # # => ["Le mot de passe doit contenir au moins 8 caractères"]
+    # # => ["Le mot de passe doit contenir au moins 12 caractères", ...]
     # ```
     def self.validate(plain_password : String) : Array(String)
       errors = [] of String
-      errors << "Le mot de passe ne peut pas être vide" if plain_password.empty?
-      errors << "Le mot de passe doit contenir au moins 8 caractères" if plain_password.size < 8
-      errors << "Le mot de passe doit contenir au moins une majuscule" unless plain_password.matches?(/[A-Z]/)
-      errors << "Le mot de passe doit contenir au moins un chiffre" unless plain_password.matches?(/[0-9]/)
+      errors << "Le mot de passe ne peut pas être vide." if plain_password.empty?
+      errors << "Le mot de passe doit contenir au moins #{MIN_LENGTH} caractères." if plain_password.size < MIN_LENGTH
+      errors << "Le mot de passe doit contenir au moins une lettre majuscule." unless plain_password.matches?(/[A-Z]/)
+      errors << "Le mot de passe doit contenir au moins une lettre minuscule." unless plain_password.matches?(/[a-z]/)
+      errors << "Le mot de passe doit contenir au moins un chiffre." unless plain_password.matches?(/[0-9]/)
+      errors << "Le mot de passe doit contenir au moins un caractère spécial (ex. ! @ # $ % & * -)." unless plain_password.matches?(/[^A-Za-z0-9]/)
       errors
     end
 
